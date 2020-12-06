50415d3e6b434507af1563d23d059051ebbc30cf
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-12-06 22:26:50 +0100

    Update MainTime.java
    Lade till att den extends thread så att den kör i en egen tråd!

--- src/projektet/MainTime.java
+++ src/projektet/MainTime.java
@@ -1,26 +1,27 @@
 package projektet;
 
-public class MainTime {
+public class MainTime extends Thread{
 
 	private int hours = 10;
 	private int minutes = 0;
 	private int seconds;
 
 	public void startCounting() {
-		for(seconds = 0; seconds <= 60; seconds++) {
+		for (seconds = 0; seconds <= 60; seconds++) {
 			if (seconds == 60) {
 				minutes++;	
-				seconds = 0;							
-			} else if(minutes == 60) {
+				minutes++;
+				seconds = 0;
+			} else if (minutes == 60) {
 				minutes = 0;
 				hours++;
-												
+
 			} else if (hours == 24) {
 				hours = 0;
 			}
-						
+
 			System.out.println(hours + ":" + minutes + ":" + seconds);
-			
+
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
