74554736de424bbc410b84704ea3a2fb5cf5cbcc
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-12-25 13:46:31 +0100

    Create Competitor.java

---
+++ src/projektet/Competitor.java
@@ -0,0 +1,26 @@
+package projektet;
+
+public class Competitor {
+	
+	private int nr;
+	private String name;
+	
+	public int getNr() {
+		return nr;
+	}
+	
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
