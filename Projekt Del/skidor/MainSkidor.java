3824d5c40a46e2b95d1636002f7092bb03757092
JennyGöransson <goransson.jenny@gmail.com>
2021-01-05 21:06:39 +0100

    Update MainSkidor.java

--- Projekt Del/skidor/MainSkidor.java
+++ Projekt Del/skidor/MainSkidor.java
@@ -1,5 +1,17 @@
 package skidor;
 
-public class MainSkidor {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainSkidor extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        LoginStage.show();
+    }
 
 }
