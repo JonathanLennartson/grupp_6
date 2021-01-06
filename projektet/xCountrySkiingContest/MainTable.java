a1471ef10f36430b10a96c7d0d1de45c48b27c29
WardenGrey <58569384+WardenGrey@users.noreply.github.com>
2021-01-06 11:25:46 +0100

    Revert "Merge branch 'main' of https://github.com/JonathanLennartson/grupp_6 into main"
    This reverts commit 1ec5cdfb70e6ece1ddeb462ca11fb5db4711eb6a, reversing
    changes made to f032ef27dca9e9e35c36f5467e2caa8485be743b.

--- projektet/xCountrySkiingContest/MainTable.java
+++ projektet/xCountrySkiingContest/MainTable.java
@@ -1,232 +1,244 @@
 package xCountrySkiingContest;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
-
-public class MainTable {
-
-    static TableView<Data> table;
-    static ObservableList<Data> tvObservableList;
-
-    
-	public static void show() {
-        Stage stage = new Stage();
-    	table = new TableView<>();
-    	tvObservableList = FXCollections.observableArrayList();    	
-    	
-    	stage.setTitle("Skidtävling!!");
-
 public class MainTable extends Application {
 
-    private final TableView<Data> table = new TableView<>();
-    private final ObservableList<Data> tvObservableList = FXCollections.observableArrayList();
+	private final TableView<Data> table = new TableView<>();
+	private final ObservableList<Data> tvObservableList = FXCollections.observableArrayList();
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	BorderPane jaktPane, massPane, individPane;
+	Stage window;
+	Scene mainScene, individScene, masScene, jaktScene;
+	ComboBox<String> menu;
+	Button selectMenuButton;
 
-    @Override
-    public void start(Stage stage) {
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-        stage.setTitle("Skidtävling!!");
+	@Override
+	public void start(Stage primaryStage) {
 
-        stage.setWidth(600);
-        stage.setHeight(600);
+		window = primaryStage;
 
-        setTableappearance();
+		menu = new ComboBox<>();
+		menu.getItems().addAll("Individuell Start", "Masstart", "Jaktstart");
+		menu.setPromptText("V�lj Din Start-Typ");
 
-        fillTableObservableListWithSampleData();
-        table.setItems(tvObservableList);
-        
-        addButtonToTable();
-        
-        addLapButtonToTable();
+		selectMenuButton = new Button("V�lj");
+		selectMenuButton.setOnAction(e -> menuButtonClicked());
 
-        TableColumn<Data, Integer> colId = new TableColumn<>("StartNummer");
-        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		window.setTitle("Skidtävling!!");
+		window.setWidth(600);
+		window.setHeight(600);
 
-        TableColumn<Data, String> colName = new TableColumn<>("Namn");
-        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        
-        TableColumn<Data, Integer> colTime = new TableColumn<>("Tid");
-        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		setTableappearance();
 
-        table.getColumns().addAll(colId, colName, colTime);
+		fillTableObservableListWithSampleData();
+		table.setItems(tvObservableList);
 
-        
+		addButtonToTable();
 
-        Scene scene = new Scene(new Group(table));
-        scene.getStylesheets().add(getClass().getResource("design.css").toExternalForm());
-        stage.setScene(scene);
-        stage.show();
-    }
+		addLapButtonToTable();
 
-    private static void setTableappearance() {
-    private void setTableappearance() {
+		TableColumn<Data, Integer> colId = new TableColumn<>("StartNummer");
+		colId.setCellValueFactory(new PropertyValueFactory<>("id"));
 
-        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        table.setPrefWidth(600);
-        table.setPrefHeight(600);
-    }
+		TableColumn<Data, String> colName = new TableColumn<>("Namn");
+		colName.setCellValueFactory(new PropertyValueFactory<>("name"));
 
+		TableColumn<Data, Integer> colTime = new TableColumn<>("Tid");
+		colId.setCellValueFactory(new PropertyValueFactory<>("id"));
 
-    private static void fillTableObservableListWithSampleData() {
+		table.getColumns().addAll(colId, colName, colTime);
 
-    private void fillTableObservableListWithSampleData() {
+		BorderPane layout = new BorderPane();
+		HBox menuLayout = new HBox(40);
 
+		menuLayout.getChildren().addAll(menu, selectMenuButton);
 
-        tvObservableList.addAll(new Data(1, "Rosie Brennan"),
-                                new Data(2, "Tatiana Sorina"), 
-                                new Data(3, "Therese Johaug"), 
-                                new Data(4, "Natalia Nepryaeva"),
-                                new Data(5, "Nadine Fähndrich"),
-                                new Data(6, "Anamarija Lampic"),
-                                new Data(7, "Ebba Andersson"),
-                                new Data(8, "Jessica Diggins"),
-                                new Data(9, "Yulia Stupak"),
-                                new Data(10, "Frida Karlsson "));
-    }
+		menuLayout.setPadding(new Insets(20, 20, 20, 20));
+		layout.setTop(menuLayout);
+		layout.setCenter(table);
 
+		mainScene = new Scene(layout, 500, 200);
+		window.setOnCloseRequest(e -> {
+			e.consume();
+			closeProgram();
+		});
 
-    private static void addButtonToTable() {
-        
+		mainScene.getStylesheets().add(getClass().getResource("design.css").toExternalForm());
+		window.setScene(mainScene);
+		window.show();
+	}
+
+	private void closeProgram() {
+		Boolean answer = ConfirmBox.display("Confirm Exit", "Are you sure you want to exit? ");
+		if (answer) {
+			window.close();
+		}
+	}
+
+	private void menuButtonClicked() {
+		menu.getValue();
+		if (menu.getValue() == "Individuell Start") {
+			individuellStart();
+		} else if (menu.getValue() == "Jaktstart") {
+			jaktStart();
+		} else if (menu.getValue() == "Masstart") {
+			masStart();
+		}
+	}
+
+	private void masStart() {
+
+	}
+
+	private void individuellStart() {
+
+	}
+
+	private void jaktStart() {
+
+	}
+
+	private void setTableappearance() {
+		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		table.setPrefWidth(600);
+		table.setPrefHeight(600);
+	}
+
+	private void fillTableObservableListWithSampleData() {
+
+		tvObservableList.addAll(new Data(1, "Rosie Brennan"), new Data(2, "Tatiana Sorina"),
+				new Data(3, "Therese Johaug"), new Data(4, "Natalia Nepryaeva"), new Data(5, "Nadine Fähndrich"),
+				new Data(6, "Anamarija Lampic"), new Data(7, "Ebba Andersson"), new Data(8, "Jessica Diggins"),
+				new Data(9, "Yulia Stupak"), new Data(10, "Frida Karlsson "));
+	}
+
+	private void addButtonToTable() {
 		TableColumn<Data, Void> colBtn = new TableColumn("Start/Stopp");
 
-   
-        Callback<TableColumn<Data, Void>, TableCell<Data, Void>> cellFactory = new Callback<TableColumn<Data, Void>, TableCell<Data, Void>>() {
-            @Override
-            public TableCell<Data, Void> call(final TableColumn<Data, Void> param) {
-                final TableCell<Data, Void> cell = new TableCell<Data, Void>() {
+		Callback<TableColumn<Data, Void>, TableCell<Data, Void>> cellFactory = new Callback<TableColumn<Data, Void>, TableCell<Data, Void>>() {
+			@Override
+			public TableCell<Data, Void> call(final TableColumn<Data, Void> param) {
+				final TableCell<Data, Void> cell = new TableCell<Data, Void>() {
 
-                    private final Button btn = new Button("Start");
-                    
+					private final Button btn = new Button("Start");
 
-                    {
-                        btn.setOnAction((ActionEvent event) -> {
-                        	btn.setText("Stop");
-                            Data data = getTableView().getItems().get(getIndex());
-                            System.out.println("selectedData: " + data);
-                        });
-                    }
+					{
+						btn.setOnAction((ActionEvent event) -> {
+							btn.setText("Stop");
+							Data data = getTableView().getItems().get(getIndex());
+							System.out.println("selectedData: " + data);
+						});
+					}
 
-                    @Override
-                    public void updateItem(Void item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            setGraphic(btn);
-                        }
-                    }
-                };
-                return cell;
-            }
-        };
-        
-        
-        
+					@Override
+					public void updateItem(Void item, boolean empty) {
+						super.updateItem(item, empty);
+						if (empty) {
+							setGraphic(null);
+						} else {
+							setGraphic(btn);
+						}
+					}
+				};
+				return cell;
+			}
+		};
 
-        colBtn.setCellFactory(cellFactory);
+		colBtn.setCellFactory(cellFactory);
 
-        table.getColumns().add(colBtn);
+		table.getColumns().add(colBtn);
 
-    }
-    
+	}
 
-    private static void addLapButtonToTable() {
-        
+	private void addLapButtonToTable() {
 		TableColumn<Data, Void> colLap = new TableColumn("Mellantid");
 
-    private void addLapButtonToTable() {
-        TableColumn<Data, Void> colLap = new TableColumn("Mellantid");
+		Callback<TableColumn<Data, Void>, TableCell<Data, Void>> cellFactory = new Callback<TableColumn<Data, Void>, TableCell<Data, Void>>() {
+			@Override
+			public TableCell<Data, Void> call(final TableColumn<Data, Void> param) {
+				final TableCell<Data, Void> cell = new TableCell<Data, Void>() {
 
+					private final Button btnLap = new Button("Lap");
 
-        Callback<TableColumn<Data, Void>, TableCell<Data, Void>> cellFactory = new Callback<TableColumn<Data, Void>, TableCell<Data, Void>>() {
-            @Override
-            public TableCell<Data, Void> call(final TableColumn<Data, Void> param) {
-                final TableCell<Data, Void> cell = new TableCell<Data, Void>() {
+					{
+						btnLap.setOnAction((ActionEvent event) -> {
+							Data data = getTableView().getItems().get(getIndex());
+							System.out.println("selectedData: " + data);
+							// TODO Chronometer for each cometitor.
+						});
+					}
 
-                    private final Button btnLap = new Button("Lap");
+					@Override
+					public void updateItem(Void item, boolean empty) {
+						super.updateItem(item, empty);
+						if (empty) {
+							setGraphic(null);
+						} else {
+							setGraphic(btnLap);
+						}
+					}
+				};
+				return cell;
+			}
+		};
 
-                    {
-                        btnLap.setOnAction((ActionEvent event) -> {
-                            Data data = getTableView().getItems().get(getIndex());
-                            System.out.println("selectedData: " + data);
-                            // TODO Chronometer for each cometitor.
-                        });
-                    }
+		colLap.setCellFactory(cellFactory);
 
-                    @Override
-                    public void updateItem(Void item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            setGraphic(btnLap);
-                        }
-                    }
-                };
-                return cell;
-            }
-        };
-        
-        
-        
+		table.getColumns().add(colLap);
 
-        colLap.setCellFactory(cellFactory);
+	}
 
-        table.getColumns().add(colLap);
+	public class Data {
 
-    }
+		private int id;
+		private String name;
 
-    public class Data {
+		private Data(int id, String name) {
+			this.id = id;
+			this.name = name;
+		}
 
-        private int id;
-        private String name;
+		public int getId() {
+			return id;
+		}
 
-        private Data(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
+		public void setId(int ID) {
+			this.id = ID;
+		}
 
-        public int getId() {
-            return id;
-        }
+		public String getName() {
+			return name;
+		}
 
-        public void setId(int ID) {
-            this.id = ID;
-        }
+		public void setName(String nme) {
+			this.name = nme;
+		}
 
-        public String getName() {
-            return name;
-        }
+		@Override
+		public String toString() {
+			return "id: " + id + " - " + "name: " + name;
+		}
 
-        public void setName(String nme) {
-            this.name = nme;
-        }
+	}
 
-        @Override
-        public String toString() {
-            return "id: " + id + " - " + "name: " + name;
-        }
-
-    }
-    
 }
-
-
-
