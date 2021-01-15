8cd0be242b4991e5ae20777ce2da8d4bb23e5994
JonathanLennartson <jonathan.lennartson@gmail.com>
2021-01-15 10:13:55 +0100

    Sista stora fix klart
    - JaktStart-logiken klar! Nu startar alla åkare med rätt tid.

--- Projekt Del/skidor/PursuitStartTime.java
+++ Projekt Del/skidor/PursuitStartTime.java
@@ -1,58 +1,38 @@
 package skidor;
 
-import java.util.ArrayList;
-
 public class PursuitStartTime {
 
 	public void setTotalTimeSec() {
 
 		double min;
 		double sec;
-		double totalTimeSec;
 
 		for (Competitor competitor : XMLhandler.list) {
 			String[] parts = competitor.getStopTime().split(":");
 			min = Double.valueOf(parts[0]);
 			sec = Double.valueOf(parts[1]);
-			System.out.println(min + ":" + sec);
-			totalTimeSec = (min * 60) + sec;
-			competitor.setTotalTimeSec(totalTimeSec);
+			competitor.setTotalTimeSec((min * 60) + sec);
 		}
 
 	}
 
 	public void setPursuitStartTime() {
-		
-		double fastestTime = 100000000;
-		String headStart;
-		double previousTime;
-		
+
+		double time;
+		int index = -1;
+
 		for (Competitor competitor : XMLhandler.list) {
-			
-//			previousTime = competitor.getTotalTimeSec();			
-			
-			if (fastestTime > competitor.getTotalTimeSec()) {
-				fastestTime = competitor.getTotalTimeSec();
+
+			try {
+				time = (competitor.getTotalTimeSec() - XMLhandler.list.get(index).getTotalTimeSec()) * 1000;
+			} catch (IndexOutOfBoundsException e) {
+				time = 0;
 			}
+
+			index++;
+			competitor.setHeadStart(Double.valueOf(time).longValue());
+
 		}
 
-		for (Competitor competitor : XMLhandler.list) {			
-			headStart = String.valueOf((competitor.getTotalTimeSec() - fastestTime) * 1000);
-			
-			
-			try {
-				headStart = headStart.substring(0, 4);
-			} catch (IndexOutOfBoundsException e) {
-			
-			}
-			
-			if (headStart.contains(".")) {
-				headStart = headStart.replace(".", "");
-			}
-				competitor.setHeadStart(Long.valueOf(headStart));
-				System.out.println("HeadStart: " + headStart);
-			
-		}
-		
 	}
 }
